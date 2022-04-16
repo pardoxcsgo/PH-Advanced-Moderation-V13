03a464f2546573d88ea074ed3b9a1dd5f7e12149
Can <canzade338@gmail.com>
2022-04-16 22:21:37 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿const config = {
+  "botStatus": ["canzade338","can"],
+  "prefix": ["!", "."],
+  "token": "",
+  "mongoose": "",
+  "botOwners": ["948636733589835807"],
+  "guildID": "927571227063779348",
+  
+  "emojis": {
+    "yes_name": "yes_zade",
+    "no_name": "no_zade",
+    "mute_name": "zade_mute"
+  }
+  }
+  
+  module.exports = config;
+  
