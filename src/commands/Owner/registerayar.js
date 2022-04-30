33d4f02c3f1a8bdb72c188d19ec7b3866179cba3
Can <canzade338@gmail.com>
2022-05-01 01:15:36 +0300

    Update registerayar.js

--- src/commands/Owner/registerayar.js
+++ src/commands/Owner/registerayar.js
@@ -20,7 +20,7 @@ class TeyitAyar extends Command {
     if (!args[0]) return this.client.yolla(`
 Yanlış bir argüman kullandınız!  
 Teyit formatını değiştirmek için **!teyitmod değiştir**
-Mevcut kayıt formatı **${server.GenderRegister ? ".e @Zade Can 22" : ".isim @Zade Can 22 - .e"}**`, message.author, message.channel)
+Mevcut kayıt formatı **${server.GenderRegister ? ".e @Zade Can 22" : ".isim @Zade Can 22 - Buttonlu Teyit"}**`, message.author, message.channel)
 
 if (args[0] == "değiştir") {
     if(!server.GenderRegister) {
@@ -28,14 +28,14 @@ if (args[0] == "değiştir") {
       server.save();
         this.client.yolla(`
 Kayıt formatı başarıyla değiştirildi. 
-Artık **${server.GenderRegister ? ".e @Zade Can 22" : ".isim @Zade Can 22 - .e"}** şeklinde kayıt gerçekleştirebilecek!`, message.author, message.channel);
+Artık **${server.GenderRegister ? ".e @Zade Can 22" : ".isim @Zade Can 22 - Buttonlu Teyit"}** şeklinde kayıt gerçekleştirebilecek!`, message.author, message.channel);
     } 
     else if(server.GenderRegister) {
       server.GenderRegister = false;
       server.save();
       this.client.yolla(`
 Kayıt formatı başarıyla değiştirildi. 
-Artık **${!server.GenderRegister ? ".isim @Zade Can 22 - .e" : ".e @Zade Can 22"}** şeklinde kayıt gerçekleştirebilecek!`, message.author, message.channel); 
+Artık **${!server.GenderRegister ? ".isim @Zade Can 22 - Buttonlu Teyit" : ".e @Zade Can 22"}** şeklinde kayıt gerçekleştirebilecek!`, message.author, message.channel); 
     }
   }
 }
