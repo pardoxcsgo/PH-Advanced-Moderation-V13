7580ce21f867b3f93b7329e274c93ddf30efea1e
Can <canzade338@gmail.com>
2022-05-01 19:55:49 +0300

    Buttonlu rollog eklendi.

--- src/commands/Management/rollog.js
+++ src/commands/Management/rollog.js
@@ -23,35 +23,76 @@ class Rollog2 extends Command {
         let rol = Veri.roller.sort((a, b) => b.tarih - a.tarih)
        // let liste = rol.map(x => `${x.state == "Ekleme" ? this.client.ok : this.client.no} Rol: <@&${x.rol}> Yetkili: <@${x.mod}>\nTarih: ${moment(x.tarih).format("LLL")}`)
        let liste = rol.map(x => `\`[${moment(x.tarih).format("LLL")}, ${x.state}]\` <@${x.mod}>: <@&${x.rol}>`)
-       var msg = await message.channel.send({ embeds: [new Discord.MessageEmbed().setDescription(`
+       const cancık = new Discord.MessageEmbed().setDescription(`
 ${Member} kişisinin toplam da verilmiş-alınmış ${Veri.roller.length} rollere ait bilgisi bulunmakta, rollerin bilgileri aşağıda belirttim.
 
-${liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join('\n')}`).setColor("RANDOM").setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }), url: `https://discord.com/users/${Member.id}` })]})
-        if (liste.length > 10) {
-            await msg.react(`⬅️`);
-            await msg.react(`➡️`);
-            let collector = msg.createReactionCollector((react, user) => ["⬅️", "➡️"].some(e => e == react.emoji.name) && user.id == message.member.id, { time: 200000 });
-            collector.on("collect", (react) => {
-                if (react.emoji.name == "➡️") {
-                    if (liste.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
-                    page += 1;
-                    let rollogVeri = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
-                    msg.edit({ embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`
+${liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join('\n')}`).setColor("RANDOM").setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }), url: `https://discord.com/users/${Member.id}` }) 
+const row = new Discord.MessageActionRow()
+.addComponents(
+  new Discord.MessageButton()
+    .setCustomId('ÖncekiSayfa')
+    .setLabel("Önceki Sayfa")
+    .setEmoji("⬅️")
+    .setStyle('PRIMARY'),
+    new Discord.MessageButton()
+    .setCustomId('CANCEL')
+    .setLabel("İptal")
+    .setStyle('DANGER'),      
+  new Discord.MessageButton()
+    .setCustomId('SonrakiSayfa')
+    .setLabel("Sonraki Sayfa")
+    .setEmoji("➡️")
+    .setStyle('PRIMARY'),
+);       
+       var msg = await message.channel.send({ embeds: [cancık], components: [row]})
+       var filter = (button) => button.user.id === message.author.id;
+       const collector = msg.createMessageComponentCollector({ filter, time: 30000 })
+
+if (liste.length > 10) {
+
+    collector.on('collect', async (button) => {
+        if (button.customId === "SonrakiSayfa") {
+            if (liste.slice((page + 1) * 10 - 10, (page + 1) * 10).length <= 0) return;
+            msg.edit({ components: [row] }); 
+       
+            page += 1;
+            let rollogVeri = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
+            msg.edit({ embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`
 ${Member} kişisinin toplam da verilmiş-alınmış ${Veri.roller.length} rollere ait bilgisi bulunmakta, rollerin bilgileri aşağıda belirttim.
-                    
+            
 ${rollogVeri}`).setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }), url: `https://discord.com/users/${Member.id}` })]})
-                    react.users.remove(message.author.id)
-                }
-                if (react.emoji.name == "⬅️") {
-                    if (liste.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
-                    page -= 1;
-                    let rollogVeri = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
-                    msg.edit({ embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`${Member} kişisinin toplam da verilmiş-alınmış ${Veri.roller.length} rollere ait bilgisi bulunmakta, rollerin bilgileri aşağıda belirttim.
-                    
+   
+button.deferUpdate();
+
+} else if (button.customId === "ÖncekiSayfa") {
+
+        if (liste.slice((page - 1) * 10 - 10, (page - 1) * 10).length <= 0) return;
+        page -= 1;
+        let rollogVeri = liste.slice(page == 1 ? 0 : page * 10 - 10, page * 10).join("\n");
+        msg.edit({ embeds: [new Discord.MessageEmbed().setColor("RANDOM").setDescription(`${Member} kişisinin toplam da verilmiş-alınmış ${Veri.roller.length} rollere ait bilgisi bulunmakta, rollerin bilgileri aşağıda belirttim.
+        
 ${rollogVeri}`).setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }), url: `https://discord.com/users/${Member.id}` })]})
-                    react.users.remove(message.author.id)
-                }
-            })
+
+button.deferUpdate();
+
+    } else if (button.customId === "CANCEL") {
+        row.components[0].setDisabled(true) 
+        row.components[1].setDisabled(true) 
+        row.components[2].setDisabled(true) 
+        msg.edit({ components: [row] }); 
+        button.reply("İşlem iptal edildi.")
+
+    }
+})
+
+collector.on("end", async (button) => {
+    row.components[0].setDisabled(true) 
+    row.components[1].setDisabled(true) 
+    row.components[2].setDisabled(true) 
+    msg.edit({ components: [row] }); 
+})
+   
+
         }
     }
 }
