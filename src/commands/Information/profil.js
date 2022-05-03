632c68792e62e60c0d4e3b15822c28e7bc3cf7a9
Can <canzade338@gmail.com>
2022-05-04 01:35:31 +0300

    Menülü profil komutu eklendi.

--- src/commands/Information/profil.js
+++ src/commands/Information/profil.js
@@ -1,6 +1,7 @@
 const Command = require("../../base/Command.js");
 const Discord = require("discord.js")
 const moment = require("moment")
+const axios = require('axios')
 require("moment-duration-format")
 moment.locale("tr")
 class Profil extends Command {
@@ -14,6 +15,8 @@ class Profil extends Command {
     async run(message, args, data) {
         if(!message.member.permissions.has("VIEW_AUDIT_LOG")) return
         let user = args.length > 0 ? message.mentions.users.first() || await this.client.client_üye(args[0]) || message.author : message.author
+     
+     
         const embed = new Discord.MessageEmbed()
         .setAuthor({ name: user.tag, iconURL: user.displayAvatarURL({ dynamic: true }) })
             .setThumbnail(user.displayAvatarURL({ dynamic: true }))
@@ -42,7 +45,51 @@ class Profil extends Command {
             embed.addField("❯ Sunucu Bilgisi", "`•` Sunucu İsmi: " + nickname + "\n`•` Katılım Tarihi: " + moment(member.joinedAt).format("LLL") + " - (" + moment(member.joinedAt).fromNow() + ")\n`•` Katılım Sırası: " + joinPos + "/" + message.guild.members.cache.size + "\n`•` Katılım Bilgisi: " + bilgi + "\n\n`•` Rolleri (" + roles.length + "): " + rolleri.join(", ") + " ", false)
             embed.setColor(member.displayHexColor)
         }
-        message.channel.send({ embeds: [embed] })
+      
+        const row = new Discord.MessageActionRow()
+        .addComponents(
+            new Discord.MessageSelectMenu()
+                .setCustomId('banner')
+                .setPlaceholder('Banner/Avatarını görüntülemek için tıkla!')
+                .addOptions([
+                    {
+                        label: 'Banner',
+                        description: 'Kullanıcının bannerini görüntülersiniz.',
+                        value: 'banner',
+                    },
+                    {
+                        label: 'Avatar',
+                        description: 'Kullanıcının avatarını görüntülersiniz.',
+                        value: 'avatar',
+                    },
+                ]),
+        );
+  let msg = await message.channel.send({ embeds: [embed], components: [row] })
+  var filter = (menu) => menu.user.id === message.author.id;
+  const collector = msg.createMessageComponentCollector({ filter, time: 30000 })
+ 
+  collector.on("collect", async (menu) => {
+     if(menu.values[0] === "avatar") {
+        menu.reply(`${user.displayAvatarURL({ dynamic: true, size: 4096 })}`)
+    } 
+    else if(menu.values[0] === "banner") {
+
+        async function bannerURL(user, client) {
+            const response = await axios.get(`https://discord.com/api/v9/users/${user}`, { headers: { 'Authorization': `Bot ${client.token}` } });
+            if(!response.data.banner) return "Kullanıcının banneri bulunmamakta!"
+            if(response.data.banner.startsWith('a_')) return `https://cdn.discordapp.com/banners/${response.data.id}/${response.data.banner}.gif?size=512`
+            else return(`https://cdn.discordapp.com/banners/${response.data.id}/${response.data.banner}.png?size=512`)
+          
+          }
+        
+          let bannerurl = await bannerURL(user.id,this.client)
+        
+          menu.reply({content: `${bannerurl}`})
+   
+
+      
+        }
+    })
     }
 }
 
