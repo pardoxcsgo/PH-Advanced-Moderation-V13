5ed59609f53eba7e2022f4a8f4795db900bc6a68
Can <canzade338@gmail.com>
2022-05-19 00:21:15 +0300

    cihaz komutu düzeltildi.

--- src/commands/Information/cihaz.js
+++ src/commands/Information/cihaz.js
@@ -13,16 +13,16 @@ class Cihaz extends Command {
 
     async run(message, args, data) {
         if (!message.member.permissions.has("VIEW_AUDIT_LOG")) return
-        let member = message.mentions.users.first() || this.client.users.cache.get(args[0])
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!args[0]) return this.client.yolla("Bir üye etiketle ve tekrardan dene!", message.author, message.channel)
         if (!member) return this.client.yolla("Belirttiğin üyeyi bulamıyorum.", message.author, message.channel)
-        if (member.presence && member.presence.status == "offline") return this.client.yolla(`\`${member.tag}\` kullanıcısı çevrimdışı olduğundan dolayı cihaz bilgisini tespit edemiyorum.`, message.author, message.channel)
+        if (member.presence.status == "offline") return this.client.yolla(`\`${member.tag}\` kullanıcısı çevrimdışı olduğundan dolayı cihaz bilgisini tespit edemiyorum.`, message.author, message.channel)
         let cihaz = ""
-        let ha = Object.keys(member.presence.status)
+        let ha = Object.keys(member.presence.clientStatus)
         if (ha[0] == "mobile") cihaz = "Mobil Telefon"
         if (ha[0] == "desktop") cihaz = "Masaüstü Uygulama"
         if (ha[0] == "web") cihaz = "İnternet Tarayıcısı"
-        message.channel.send(`\`${member.tag}\` üyesinin şu anda kullandığı cihaz: \`${cihaz}\``)
+        message.channel.send(`\`${member.user.tag}\` üyesinin şu anda kullandığı cihaz: \`${cihaz}\``)
 
     }
 }
