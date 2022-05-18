bb9e0649cd1696ba2f5119ed7e323c12b78c4967
Can <canzade338@gmail.com>
2022-05-19 00:57:27 +0300

    koruma komutu düzeltildi.

--- src/commands/Owner/koruma.js
+++ src/commands/Owner/koruma.js
@@ -1,4 +1,5 @@
 const Command = require("../../base/Command.js")
+const Discord = require("discord.js")
 let serverSettings = require("../../models/serverSettings");
 class Koruma extends Command {
     constructor(client) {
@@ -12,27 +13,115 @@ class Koruma extends Command {
     }
 
     async run(message, args, perm) {
+     
         let server = await serverSettings.findOne({
             guildID: message.guild.id
         });
-        if(!message.member.permissions.has("ADMINISTRATOR")) return
-        if(!args[0]) {
-            message.guild.roles.cache.get(this.client.config.roles.Führer).setPermissions(0) // Führer
-            message.guild.roles.cache.get(this.client.config.roles.Owner).setPermissions(0) // Owner
-            message.guild.roles.cache.get(this.client.config.roles.Ceo).setPermissions(0) // CEO    
-            message.guild.roles.cache.get(this.client.config.roles.Bael).setPermissions(0) // Bael
-            message.guild.roles.cache.get(this.client.config.roles.Çiftyıldız).setPermissions(0) // Çift Yıldız
-            message.channel.send(`${this.client.emojis.cache.find(x => x.name === this.client.config.emojis.no_name)} Sunucumuzun tüm rolleri güvenlik açısından devre dışı bırakılmıştır.`)
+        if(!server.BotOwner.includes(message.author.id) && !server.GuildOwner.includes(message.author.id)) return
+   
+       
+       const cancık = new Discord.MessageEmbed()
+       .setColor("RANDOM")
+       .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) })
+       .setDescription(`
+${message.author}, Sunucudaki yetkileri açıp kapatmak için aşağıdaki butonları seçin. Kapatıp açılacak yetkiler sırasıyla:
+
+<@&${server.ManagementRoles[0]}>: [8](https://discordapi.com/permissions.html#8)
+<@&${server.ManagementRoles[1]}>: [402784260](https://discordapi.com/permissions.html#402784260)
+<@&${server.ManagementRoles[2]}>: [268436608](https://discordapi.com/permissions.html#268436608)
+<@&${server.ManagementRoles[3]}>: [128](https://discordapi.com/permissions.html#128)
+<@&${server.ManagementRoles[4]}>: [128](https://discordapi.com/permissions.html#128)
+
+olarak belirlenmiştir.`)
+
+        const row = new Discord.MessageActionRow()
+        .addComponents(
+          new Discord.MessageButton()
+            .setCustomId('Aç')
+            .setLabel("Aç")
+            .setStyle('PRIMARY'),
+            new Discord.MessageButton()
+            .setCustomId('Kapat')
+            .setLabel("Kapat")
+            .setStyle('DANGER'),      
+          new Discord.MessageButton()
+            .setCustomId('İptal')
+            .setLabel("İptal")
+            .setStyle('DANGER'),
+        );       
+               var msg = await message.channel.send({ embeds: [cancık], components: [row]})
+               var filter = (button) => button.user.id === message.author.id;
+               const collector = msg.createMessageComponentCollector({ filter, time: 30000 })
+  
+          
+          collector.on("collect", async(button) => {
+
+            if(button.customId === "Aç") {
+
+            row.components[0].setDisabled(true)
+
+            msg.edit({ components: [row] })
+
+            message.guild.roles.cache.get(server.ManagementRoles[0]).setPermissions([Discord.Permissions.FLAGS.ADMINISTRATOR]);//Führer
+            message.guild.roles.cache.get(server.ManagementRoles[1]).setPermissions([Discord.Permissions.FLAGS.MANAGE_ROLES, Discord.Permissions.FLAGS.BAN_MEMBERS, Discord.Permissions.FLAGS.MANAGE_NICKNAMES, Discord.Permissions.FLAGS.MENTION_EVERYONE]);//Owner 
+            message.guild.roles.cache.get(server.ManagementRoles[2]).setPermissions([Discord.Permissions.FLAGS.VIEW_CHANNEL, Discord.Permissions.FLAGS.MANAGE_CHANNELS, Discord.Permissions.FLAGS.VIEW_AUDIT_LOG]);//CEO
+            message.guild.roles.cache.get(server.ManagementRoles[3]).setPermissions([Discord.Permissions.FLAGS.VIEW_AUDIT_LOG]);//Bael
+            message.guild.roles.cache.get(server.ManagementRoles[4]).setPermissions([Discord.Permissions.FLAGS.VIEW_AUDIT_LOG]);//Çift Yıldız
+            
+             const aç = new Discord.MessageEmbed()
+         .setColor("RANDOM")
+         .setDescription(`**Açılan Yetkiler Sırasıyla Aşağıda Belirtilmiştir.**\n
+<@&${server.ManagementRoles[0]}>: [8](https://discordapi.com/permissions.html#8)
+<@&${server.ManagementRoles[1]}>: [402784260](https://discordapi.com/permissions.html#402784260)
+<@&${server.ManagementRoles[2]}>: [268436608](https://discordapi.com/permissions.html#268436608)
+<@&${server.ManagementRoles[3]}>: [128](https://discordapi.com/permissions.html#128)
+<@&${server.ManagementRoles[4]}>: [128](https://discordapi.com/permissions.html#128)
+        `)
+            button.reply({ embeds: [aç]})
+
+            } else if(button.customId === "Kapat") {
+             row.components[1].setDisabled(true)
+             msg.edit({ components: [row] })
+
+            message.guild.roles.cache.get(server.ManagementRoles[0]).setPermissions([]);//Führer
+            message.guild.roles.cache.get(server.ManagementRoles[1]).setPermissions([]);//Owner
+            message.guild.roles.cache.get(server.ManagementRoles[2]).setPermissions([]);//CEO
+            message.guild.roles.cache.get(server.ManagementRoles[3]).setPermissions([]);//Bael
+            message.guild.roles.cache.get(server.ManagementRoles[4]).setPermissions([]);//Çift Yıldız
+
+             const kapat = new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setDescription(`**Kapatılan Yetkiler Sırasıyla Aşağıda Belirtilmiştir.**\n
+<@&${server.ManagementRoles[0]}>: [PERMLER](https://discordapi.com/permissions.html#PERMLER)
+<@&${server.ManagementRoles[1]}>: [PERMLER](https://discordapi.com/permissions.html#PERMLER)
+<@&${server.ManagementRoles[2]}>: [PERMLER](https://discordapi.com/permissions.html#PERMLER)
+<@&${server.ManagementRoles[3]}>: [PERMLER](https://discordapi.com/permissions.html#PERMLER)
+<@&${server.ManagementRoles[4]}>: [PERMLER](https://discordapi.com/permissions.html#PERMLER)
+           `)
+               button.reply({ embeds: [kapat]})
+
+            } else if(button.customId === "İptal") {
+              row.components[0].setDisabled(true)
+              row.components[1].setDisabled(true)
+              row.components[2].setDisabled(true)
+
+              msg.edit({ components: [row] })
+
+              button.reply("İşlem iptal edildi")
+            }
+
+          })
+          
+          collector.on("end", async(button) => {
+              row.components[0].setDisabled(true)
+              row.components[1].setDisabled(true)
+              row.components[2].setDisabled(true)
+
+              msg.edit({ components: [row] })
+
+          })
+
         }
-        if(args[0] == "kapat") {
-            message.guild.roles.cache.get(this.client.config.roles.Führer).setPermissions(8)
-            message.guild.roles.cache.get(this.client.config.roles.Owner).setPermissions(2684350149)
-            message.guild.roles.cache.get(this.client.config.roles.Ceo).setPermissions(268436608)
-            message.guild.roles.cache.get(this.client.config.roles.Bael).setPermissions(128)
-            message.guild.roles.cache.get(this.client.config.roles.Çiftyıldız).setPermissions(128)
-            message.channel.send(`${this.client.emojis.cache.find(x => x.name === this.client.config.emojis.yes_name)} Sunucumuzun yönetici rolleri tekrardan aktif hale getirilmiştir.`)
-        } 
-    }
 }
 
 module.exports = Koruma;
