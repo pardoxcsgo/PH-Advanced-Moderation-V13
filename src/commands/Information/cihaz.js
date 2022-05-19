b3c12099c91aeb4ba870ad6ea2428145b5e5269e
Can <canzade338@gmail.com>
2022-05-19 16:40:24 +0300

    Update cihaz.js

--- src/commands/Information/cihaz.js
+++ src/commands/Information/cihaz.js
@@ -16,7 +16,7 @@ class Cihaz extends Command {
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
         if (!args[0]) return this.client.yolla("Bir üye etiketle ve tekrardan dene!", message.author, message.channel)
         if (!member) return this.client.yolla("Belirttiğin üyeyi bulamıyorum.", message.author, message.channel)
-        if (member.presence.status == "offline") return this.client.yolla(`\`${member.tag}\` kullanıcısı çevrimdışı olduğundan dolayı cihaz bilgisini tespit edemiyorum.`, message.author, message.channel)
+        if (member.presence.status == "offline") return this.client.yolla(`\`${member.user.tag}\` kullanıcısı çevrimdışı olduğundan dolayı cihaz bilgisini tespit edemiyorum.`, message.author, message.channel)
         let cihaz = ""
         let ha = Object.keys(member.presence.clientStatus)
         if (ha[0] == "mobile") cihaz = "Mobil Telefon"
